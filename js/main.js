f8ede8442914ebb62d3a74453c24a59194a3f513
Mariam Aptsiauri <72245573+mariamapciaurii@users.noreply.github.com>
2021-05-24 17:13:35 +0400

    adding home section and styling

--- js/main.js
+++ js/main.js
@@ -24,15 +24,15 @@ window.addEventListener("scroll", scrollActive)
 
 function scrollActive(){
     const  scrollY = window.pageYOffset
-    sections.forEach(current =>{
+    sections.forEach(current=>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute("id")
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector(".nav__menu a [href*="+ sectionId + "]").classList.add("active")
+            document.querySelector(".nav__menu a[href*="+ sectionId + "]").classList.add("active")
         }else{
-            document.querySelector(".nav__menu a [href*="+ sectionId + "]").classList.remove("active")
+            document.querySelector(".nav__menu a[href*="+ sectionId + "]").classList.remove("active")
         }
     })
 }
